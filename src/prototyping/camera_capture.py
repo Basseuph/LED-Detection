c2c0c557065169d3198449551a6a256ceceb7c23
olfarmer <mail@clemens.jann.xyz>
2021-10-28 15:51:13 +0200

    Fix dummy for camera capture

--- src/prototyping/camera_capture.py
+++ src/prototyping/camera_capture.py
@@ -1,43 +1,44 @@
+from time import sleep
+
 from cv2 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
 from src.prototyping.destinationPoints import show_leds
 
-index = 1
-done = False
-messages = ["Top left", "Top right", "Bottom left", "Bottom right"]
 corners = []
-
+img = cv2.imread("./resources/test3.jpg")
 
 def capture_camera():
-    cap = cv2.VideoCapture(0)
+    #cap = cv2.VideoCapture(0)
 
 
-    cv2.namedWindow("Camera")
+    cv2.imshow("Camera", img)
+
+    #cv2.namedWindow("Camera")
     cv2.setMouseCallback("Camera", on_click)
 
-    print("Please click on the " + messages[0] + " corner")
+    print("Please click on the first corner")
 
-    while True:
-        ret, img = cap.read()
-        cv2.imshow("Camera", img)
 
-        if done:
-            show_leds(corners, img)
-            break
-
+    cv2.waitKey(1000000)
+    #cap.realease()
+    cv2.destroyAllWindows()
 
 def on_click(event, x, y, flags, param):
-    corner = [x, y]
-    corners.append(corner)
+    if event == cv2.EVENT_LBUTTONDOWN:
+        corner = [x, y]
+        corners.append(corner)
+        print("Please click on the next corner if not done yet")
 
-    if index < 3:
-        print("Please click on the " + messages[index] + " corner")
-        index += 1
-    else:
-        print("Done!")
-        done = True
+        if len(corners) == 4:
+            corners[2], corners[3] = corners[3], corners[2]
+            cv2.polylines(img, [np.array(corners)], False, (255, 0, 0), 4)
+            cv2.imshow("Camera", img)
+            corners[2], corners[3] = corners[3], corners[2]
+            show_leds(corners, img)
+
+
 
 
 capture_camera()
