073db9e25a064809b469e58d0bec89c1f0436b14
Clemens Jann <mail@clemens.jann.xyz>
2021-11-01 11:20:37 +0100

    Change capture_camera to detect led status on video file or camera
    With the provided video file in resources it is now possible to detect the status of the LEDs. First the corners have to be selected in the usual order. Afterwards the status will be printed in the console whenever it changes. The video will be looped.

--- src/prototyping/camera_capture.py
+++ src/prototyping/camera_capture.py
@@ -4,25 +4,53 @@ from cv2 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-from src.prototyping.destinationPoints import show_leds
+from src.prototyping.destinationPoints import show_leds, detect_status
 
 corners = []
 img = cv2.imread("./resources/test3.jpg")
 
 def capture_camera():
     #cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture("./resources/piOnOff.mp4")
 
 
-    cv2.imshow("Camera", img)
 
-    #cv2.namedWindow("Camera")
+    cv2.namedWindow("Camera")
     cv2.setMouseCallback("Camera", on_click)
-
     print("Please click on the first corner")
+
+    old_s1 = False
+    old_s2 = False
+
+    while True:
+        ret, rec = cap.read()
+
+        #Loop video if finished
+        if not ret:
+            cap.release()
+            cap = cv2.VideoCapture("./resources/piOnOff.mp4")
+            continue
+
+        global img
+        img = rec
+        cv2.imshow("Camera", rec)
+
+        if len(corners) == 4:
+            s1, s2 = detect_status(corners, img)
+
+            if s1 != old_s1:
+                print("LED1 on: " + str(s1))
+                old_s1 = s1
+            if s2 != old_s2:
+                print("LED2 on: " + str(s2))
+                old_s2 = s2
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 
     cv2.waitKey(1000000)
-    #cap.realease()
+    cap.realease()
     cv2.destroyAllWindows()
 
 def on_click(event, x, y, flags, param):
@@ -31,12 +59,6 @@ def on_click(event, x, y, flags, param):
         corners.append(corner)
         print("Please click on the next corner if not done yet")
 
-        if len(corners) == 4:
-            corners[2], corners[3] = corners[3], corners[2]
-            cv2.polylines(img, [np.array(corners)], False, (255, 0, 0), 4)
-            cv2.imshow("Camera", img)
-            corners[2], corners[3] = corners[3], corners[2]
-            show_leds(corners, img)
 
 
 
