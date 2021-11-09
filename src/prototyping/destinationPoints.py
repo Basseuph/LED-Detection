Add new reference and fix scaling

src/prototyping/destinationPoints.py
@@ -55,8 +55,8 @@ def unwarp(img, src, dst):
 
     #f, (ax1, ax2) = plt.subplots(1, 2)
     #ax1.imshow(img)
-    x = [src[0][0], src[2][0], src[3][0], src[1][0], src[0][0]]
-    y = [src[0][1], src[2][1], src[3][1], src[1][1], src[0][1]]
+    #x = [src[0][0], src[2][0], src[3][0], src[1][0], src[0][0]]
+    #y = [src[0][1], src[2][1], src[3][1], src[1][1], src[0][1]]
     #ax1.plot(x, y, color='yellow', linewidth=3)
     #ax1.set_ylim([h, 0])
     #ax1.set_xlim([0, w])
@@ -67,10 +67,11 @@ def unwarp(img, src, dst):
     return un_warped, H
 
 def detect_status(corners, img):
+    img = cv2.rotate(img, cv2.ROTATE_180)
     destination_points, h, w = get_destination_points(corners)
-    un_warped, H = unwarp(img, np.float32(corners), destination_points)
-    cropped = un_warped[0:h, 0:w]
-    led1, led2 = getRoiByImage(cropped, H)
+    #un_warped, H = unwarp(img, np.float32(corners), destination_points)
+    h, _ = cv2.findHomography(np.float32(corners), destination_points, method=cv2.RANSAC, ransacReprojThreshold=3.0)
+    led1, led2 = get_roi_by_dest_corners(img, h, corners)
 
     led1 = cv2.cvtColor(led1, cv2.COLOR_RGB2HSV)
     led2 = cv2.cvtColor(led2, cv2.COLOR_RGB2HSV)
@@ -78,21 +79,22 @@ def detect_status(corners, img):
     mean1 = led1[..., 2].mean()
     mean2 = led2[..., 2].mean()
 
-    return mean1 > 200, mean2 > 200
+    return mean1 > 220, mean2 > 220
 
 def show_leds(corners, img):
 
     destination_points, h, w = get_destination_points(corners)
 
-
     un_warped, H = unwarp(img, np.float32(corners), destination_points)
 
+    get_roi_by_dest_corners(img, H, corners)
+
     cropped = un_warped[0:h, 0:w]
-    f, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 8))
+    # f, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 8))
     # f.subplots_adjust(hspace=.2, wspace=.05)
-    ax1.imshow(un_warped)
-    ax2.imshow(cropped)
-    plt.show()
+    # ax1.imshow(un_warped)
+    # ax2.imshow(cropped)
+    # plt.show()
 
     #cropped = cv2.rotate(cropped, cv2.ROTATE_180)
 
@@ -100,11 +102,18 @@ def show_leds(corners, img):
 
 
 
-    r_p1 = getRoiByImage(cropped, H)
-    img = cv2.circle(img, r_p1[0:1], 5, (0, 0, 255), 3)
+    # r_p1 = getRoiByImage(cropped, H)
+    #img = cv2.circle(img, r_p1[0:1], 5, (0, 0, 255), 3)
 
-#corners = [(70, 42), (1115, 129), (30, 695), (1077, 766)] # realTraining2
-#corners = [(228, 346), (482, 163), (479, 487), (688, 247)] #angleTest.jpg
-#corners = [(111,307), (437, 300), (109, 525), (440, 517)] # realTest1
-#img = cv2.imread('resources/realTest1.jpg')
-#detect_status(corners, img)
+
+if __name__ == '__main__':
+    #corners = [(83, 32), (1093, 104), (43, 689), (1028, 757)]  # realTraining 2
+    #corners = [(223, 227), (654, 257), (205, 510), (636, 540)] #realTraining22
+    corners = [(228, 346), (482, 163), (479, 487), (688, 247)] #angleTest.jpg
+    #corners = [(111,307), (437, 300), (107, 525), (440, 517)] # realTest1
+    #corners = np.array([[871, 884], [2573, 1012], [795, 1988], [2476, 2109]])  # realTraining3 rotated
+    #corners = np.array([[871, 887], [2555, 1004], [801, 1992], [2484, 2109]])  # realTraining3 rotated, idealised
+    img = cv2.imread('resources/angleTest.jpg')
+    #img = cv2.rotate(img, cv2.ROTATE_180)
+    show_leds(corners, img)
+    #detect_status(corners, img)

src/prototyping/roi_dedection.py
@@ -57,29 +57,47 @@ def getRoiByImage(img, H):
     #cv2.waitKey(0)
     return led1, led2
 
-def get_roi_by_dest_corners(img, H):
-    measured_corners = np.array([[1, 69, 1], [12, 105, 1], [1, 115, 1], [9, 147, 1]])
+
+def get_roi_by_dest_corners(img, H, crn_pts_src):
+    # 1024x768 realTraining22, cropped 432x283
+    # coordinates from realTraining22, relative to top left corner
+    measured_corners = np.array([[0, 32, 1], [5, 43, 1], [0, 51, 1], [4, 62, 1]])
+    measured_hw = (432, 283)
+
+    scale_x = abs(crn_pts_src[0][0] - crn_pts_src[1][0]) / measured_hw[0]
+    scale_y = abs(crn_pts_src[0][1] - crn_pts_src[2][1]) / measured_hw[1]
 
     hinv = numpy.linalg.inv(H)
 
     for corner in measured_corners:
-        corner[0] = corner[0]
-        corner[1] = corner[1]
-
+        corner[0] = corner[0] * scale_x
+        corner[1] = corner[1] * scale_y
         t_corner = np.matmul(hinv, corner)
         corner[0] = t_corner[0]
         corner[1] = t_corner[1]
 
     transformed_corners = measured_corners.astype(int)
 
-
-    transformed_corners = transformed_corners.astype(int)
-
-    led1 = img[transformed_corners[0][1]:transformed_corners[1][1], transformed_corners[0][0]:transformed_corners[1][0]]
-    led2 = img[transformed_corners[2][1]:transformed_corners[3][1], transformed_corners[2][0]:transformed_corners[3][0]]
-
+    led1, led2 = leds_by_corners(img, transformed_corners)
 
     #cv2.imshow("Led1", led1)
     #cv2.imshow("Led2", led2)
     #cv2.waitKey(0)
+    return led1, led2
+
+
+def leds_by_corners(img, corners):
+    """
+    returns the led roi
+    :param img: The whole image of the board
+    :param corners: The corners selected by the user
+    :return:
+    """
+    # flips the orientation if needed
+    led1_coors = (corners[0], corners[1]) if corners[0][1] < corners[1][1] else (corners[1], corners[0])
+    led2_coors = (corners[2], corners[3]) if corners[2][1] < corners[3][1] else (corners[3], corners[2])
+
+    led1 = img[led1_coors[0][1]:led1_coors[1][1], led1_coors[0][0]:led1_coors[1][0]]
+    led2 = img[led2_coors[0][1]:led2_coors[1][1], led2_coors[0][0]:led2_coors[1][0]]
+
     return led1, led2