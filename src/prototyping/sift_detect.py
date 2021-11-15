7cb076bdfa2477b15534ea7d8cef4df54f8db3b7
olfarmer <mail@clemens.jann.xyz>
2021-11-15 14:53:56 +0100

    Change sift_detect to accept parameters and return homography matrix

--- src/prototyping/sift_detect.py
+++ src/prototyping/sift_detect.py
@@ -7,23 +7,13 @@ from matplotlib import pyplot as plt
 
 from src.prototyping.roi_dedection import get_roi_by_dest_corners
 
-if __name__ == '__main__':
-    refFilename = os.path.join("referenceCropped.jpg")
-    imFilename = os.path.join("resources", "moreUniqueExamples", "4.jpg")
 
-    # Read reference image
-    print("Reading reference image : ", refFilename)
-    img1 = cv2.imread(refFilename, cv2.IMREAD_COLOR)
-
-    # Read image to be aligned
-    print("Reading image to align : ", imFilename)
-    img2 = cv2.imread(imFilename, cv2.IMREAD_COLOR)
-
+def homography_by_sift(ref_img, im_img):
     # Initiate SIFT detector
     sift = cv2.SIFT_create()
     # find the keypoints and descriptors with SIFT
-    kp1, des1 = sift.detectAndCompute(img1, None)
-    kp2, des2 = sift.detectAndCompute(img2, None)
+    kp1, des1 = sift.detectAndCompute(ref_img, None)
+    kp2, des2 = sift.detectAndCompute(im_img, None)
     FLANN_INDEX_KDTREE = 1
     index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
     search_params = dict(checks=50)
@@ -35,6 +25,7 @@ if __name__ == '__main__':
         if m.distance < 0.80 * n.distance:
             good.append(m)
 
+    M = None
     if len(good) > 10:
         src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
         dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
@@ -43,10 +34,10 @@ if __name__ == '__main__':
         get_roi_by_dest_corners(img2, M)
 
         matchesMask = mask.ravel().tolist()
-        h, w, d = img1.shape
+        h, w, d = ref_img.shape
         pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
         dst = cv2.perspectiveTransform(pts, M)
-        img2 = cv2.polylines(img2, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
+        im_img = cv2.polylines(im_img, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
     else:
         print("Not enough matches are found - {}/{}".format(len(good), 10))
         matchesMask = None
@@ -55,5 +46,11 @@ if __name__ == '__main__':
                        singlePointColor=None,
                        matchesMask=matchesMask,  # draw only inliers
                        flags=2)
-    img3 = cv2.drawMatches(img1, kp1, img2, kp2, good, None, **draw_params)
+    img3 = cv2.drawMatches(ref_img, kp1, im_img, kp2, good, None, **draw_params)
     plt.imshow(img3, 'gray'), plt.show()
+    return M
+
+
+if __name__ == '__main__':
+    homography_by_sift(cv2.imread(os.path.join("referenceCropped.jpg"), cv2.IMREAD_COLOR),
+                       cv2.imread(os.path.join("resources", "realTraining2.jpg"), cv2.IMREAD_COLOR))
