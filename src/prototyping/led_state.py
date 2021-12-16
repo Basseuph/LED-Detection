16cdcd59ce4b6759e63bd155f2c1425673c7b17e
Jan-Philipp <jan-philipp.lamers@uni-ulm.de>
2021-12-16 13:55:06 +0100

    feat: LED name

--- src/prototyping/led_state.py
+++ src/prototyping/led_state.py
@@ -42,7 +42,7 @@ def avg(hist: [[int]]) -> int:
 class LedStateDetector:
     _counter = 0
 
-    def __init__(self, bbox, colors=None):
+    def __init__(self, bbox, name: str = None, colors: [str] = None):
         """
         Bounding box should be (left, top, right, bottom).
         Current LED state can be checked with is_on.
@@ -50,6 +50,7 @@ class LedStateDetector:
         The time since the last state change can be checked using passed_time.
         None is used as an undefined state for color, is_on and passed_time.
         :param bbox: the bounding box to define the LED's location.
+        :param name: the name / identification for this LED.
         :param colors: all colors that should be checked for on this LED.
         """
         if colors is None:
@@ -61,9 +62,13 @@ class LedStateDetector:
         self._off_histogram = None
         self._on_histogram = None
         self._colors = colors
-
-        self.id = LedStateDetector._counter
+        self._id = LedStateDetector._counter
         LedStateDetector._counter += 1
+
+        if name is None:
+            self.name = str(self._id)
+        else:
+            self.name = name
         self.is_on = None
         self.passed_time = None
         self.color = None
@@ -110,7 +115,7 @@ class LedStateDetector:
         gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         hist = cv2.calcHist([gray_img], [0], None, [256], [0, 255])
         if imshow:
-            cv2.imshow(str(self.id) + "Gray", gray_img)
+            cv2.imshow(str(self.name) + " Gray", gray_img)
         hist_avg = avg(hist)
         if len(self._on_values) == 0:
             if hist_avg in range(self._last_brightness - 10, self._last_brightness + 10) or self._last_brightness == -1:
@@ -180,7 +185,7 @@ class LedStateDetector:
                 values.append((sum(hist[lower:] + hist[0: upper]), c))
             else:
                 values.append((sum(hist[lower:upper]), c))
-        print(values)
+        print(self.name, values)
         return max(values, key=itemgetter(0))
 
 
@@ -188,32 +193,35 @@ if __name__ == '__main__':
     tests = {
         cv2.VideoCapture("resources/piOnOff.mp4"): [
             # green
-            LedStateDetector((980, 620, 1030, 660), ["red", "green", "yellow", "cyan"]),
+            LedStateDetector((980, 620, 1030, 660), name="Green", colors=["red", "green", "yellow", "cyan"]),
             # red
-            LedStateDetector((980, 660, 1050, 700), ["red", "yellow", "purple"])],
+            LedStateDetector((980, 660, 1050, 700), name="Red", colors=["red", "yellow", "purple"])],
         cv2.VideoCapture("resources/piOnOff3.mp4"): [
             # red
-            LedStateDetector((380, 955, 428, 1000), ["yellow", "red", "purple"]),
+            LedStateDetector((380, 955, 428, 1000), name="Red", colors=["yellow", "red", "purple"]),
             # green
-            LedStateDetector((429, 955, 470, 1000), ["red", "yellow", "cyan", "green"])],
+            LedStateDetector((429, 955, 470, 1000), name="Green", colors=["red", "yellow", "cyan", "green"])],
         cv2.VideoCapture("resources/piOnOff4.mp4"): [
             # red
-            LedStateDetector((90, 313, 160, 365), ["yellow", "purple", "red"]),
+            LedStateDetector((90, 313, 160, 365), name="Red", colors=["yellow", "purple", "red"]),
             # green
-            LedStateDetector((90, 366, 160, 410), ["green"])],
+            LedStateDetector((90, 366, 160, 410), name="Green", colors=["green"])],
     }
+
     for video in tests:
         leds = tests.get(video)
 
+        cv2.namedWindow('Raw', cv2.WINDOW_KEEPRATIO)
         frame_exists, frame = video.read()
         while frame_exists:
             for led in leds:
                 if led.detect(frame, True):
                     if led.is_on:
-                        print("LED", led.id, "ON", "Time passed:", led.passed_time, "Color: ", led.color)
+                        print(led.name, "ON", "Time passed:", led.passed_time, "Color: ", led.color)
                     else:
-                        print("LED", led.id, "OFF", "Time passed:", led.passed_time)
+                        print(led.name, "OFF", "Time passed:", led.passed_time)
             cv2.imshow("Raw", frame)
+            cv2.resizeWindow("Raw", 500, 500)
             if cv2.waitKey(10) == 27:
                 break
             frame_exists, frame = video.read()
